4c940377ee287d44c80ba62e9067e7520c55f6bf
Vanina Quezada <vaninaquezada@gmail.com>
2019-05-20 00:33:34 -0300

    tp1

---
+++ TP1/Entidades/Entidades/Calculadora.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Calculadora
+    {
+
+
+        public double Operar(Numero num1, Numero num2, string operador)
+        {
+            double resultado = 0;
+
+            switch (Calculadora.ValidarOperador(operador))
+            {
+                case "+":
+
+                    resultado = num1 + num2;
+                    break;
+                case "-":
+                    resultado = num1 - num2;
+                    break;
+                case "*":
+                    resultado = num1 * num2;
+                    break;
+                case "/":
+                    //if (num2 == 0)
+                    //  {
+                    resultado = double.MinValue;
+                  //  }
+                  //  else {
+                //        return num1 / num2;
+               //     }
+                    break;
+
+            }
+
+            return resultado;
+            
+        }
+
+
+        private static string ValidarOperador(string operador)
+        {
+
+            if (operador == "+" || operador == "-" || operador == "*" || operador == "/")
+            {
+                return operador;
+            }
+            else { return "+"; }
+        }
+    }
+}
